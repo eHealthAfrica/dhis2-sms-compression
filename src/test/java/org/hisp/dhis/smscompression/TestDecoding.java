c11780d935eec8088a969dd04fe1a1c4ca1ef985
Jasper Timm <jasper.timm@ehealthafrica.org>
2019-05-17 14:05:09 +0000

    Fix for the CRC calculation

--- src/test/java/org/hisp/dhis/smscompression/TestDecoding.java
+++ src/test/java/org/hisp/dhis/smscompression/TestDecoding.java
@@ -9,7 +9,6 @@ import org.hisp.dhis.smscompression.models.DeleteSMSSubmission;
 import org.hisp.dhis.smscompression.models.EnrollmentSMSSubmission;
 import org.hisp.dhis.smscompression.models.Metadata;
 import org.hisp.dhis.smscompression.models.RelationshipSMSSubmission;
-import org.hisp.dhis.smscompression.models.SMSSubmissionHeader;
 import org.hisp.dhis.smscompression.models.SimpleEventSMSSubmission;
 import org.hisp.dhis.smscompression.models.TrackerEventSMSSubmission;
 import org.junit.Test;
@@ -21,43 +20,41 @@ import junit.framework.Assert;
 
 public class TestDecoding {
 	
-	//TODO: There's an issue with the CRC check in this
-//	@Test
-//	public void testDecodeDelete() {
-//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//		String sms = "NxFceV7su3CdBalEvuenk1Ehu9bwXA1HWBqePUDHAA==";
-//		byte[] smsBytes = Base64.getDecoder().decode(sms);
-//
-//		try {
-//			String metadataJson = IOUtils.toString(new FileReader("src/test/resources/metadata.json"));
-//			Metadata meta = gson.fromJson(metadataJson, Metadata.class);
-//			SMSSubmissionReader reader = new SMSSubmissionReader();
-//			DeleteSMSSubmission subm = (DeleteSMSSubmission) reader.readSubmission(smsBytes, meta);
-//			System.out.println("Decoded SMS for Delete is: " + gson.toJson(subm));			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			Assert.fail(e.getMessage());
-//		}		
-//	}
+	@Test
+	public void testDecodeDelete() {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String sms = "NxFceV7su3CdBalEvuenk1Ehu9bwXA1HWBqePUDH5A==";
+		byte[] smsBytes = Base64.getDecoder().decode(sms);
+
+		try {
+			String metadataJson = IOUtils.toString(new FileReader("src/test/resources/metadata.json"));
+			Metadata meta = gson.fromJson(metadataJson, Metadata.class);
+			SMSSubmissionReader reader = new SMSSubmissionReader();
+			DeleteSMSSubmission subm = (DeleteSMSSubmission) reader.readSubmission(smsBytes, meta);
+			System.out.println("Decoded SMS for Delete is: " + gson.toJson(subm));			
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}		
+	}
 	
-	//TODO: There's an issue with the CRC check in this
-//	@Test
-//	public void testDecodeRelationship() {
-//		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//		String sms = "JzFceV7su3CdBalEvuenk1Ehu9bwXAbeYkAIcFGJv4pD251wzIYtRG4cKWYFgVNkDuQmQNM4uAA=";
-//		byte[] smsBytes = Base64.getDecoder().decode(sms);
-//
-//		try {
-//			String metadataJson = IOUtils.toString(new FileReader("src/test/resources/metadata.json"));
-//			Metadata meta = gson.fromJson(metadataJson, Metadata.class);
-//			SMSSubmissionReader reader = new SMSSubmissionReader();
-//			RelationshipSMSSubmission subm = (RelationshipSMSSubmission) reader.readSubmission(smsBytes, meta);
-//			System.out.println("Decoded SMS for Relationship is: " + gson.toJson(subm));			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			Assert.fail(e.getMessage());
-//		}		
-//	}	
+	@Test
+	public void testDecodeRelationship() {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String sms = "JzFceV7su3CdBalEvuenk1Ehu9bwXAbeYkAIcFGJv4pD251wzIYtRG4cKWYFgVNkDuQmQNM4uGA=";
+		byte[] smsBytes = Base64.getDecoder().decode(sms);
+
+		try {
+			String metadataJson = IOUtils.toString(new FileReader("src/test/resources/metadata.json"));
+			Metadata meta = gson.fromJson(metadataJson, Metadata.class);
+			SMSSubmissionReader reader = new SMSSubmissionReader();
+			RelationshipSMSSubmission subm = (RelationshipSMSSubmission) reader.readSubmission(smsBytes, meta);
+			System.out.println("Decoded SMS for Relationship is: " + gson.toJson(subm));			
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}		
+	}	
 	
 	@Test
 	public void testDecodeSimpleEvent() {
@@ -98,7 +95,7 @@ public class TestDecoding {
 	@Test
 	public void testDecodeEnrollment() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String sms = "GyFcVHTsu3CdBalEvuenk1Ehu9bwXAbeYkAIcFGJvveU7bcYi7OjdClb4VOQyfXzHWBqePUDHgXFR07Dv0hhcm9sZAD+jsrkwtjIAIFNtaXRoAA=";
+		String sms = "SCFceV7su3CdBalEvuenk1Ehu9bwXAbeYkAIcFGJvveU7bcYi7OjdClb4VOQyfXzHWBqePUDHgXHle7C/SGFyb2xkAPo7K5MLYyAAFNtaXRoAA==";
 		byte[] smsBytes = Base64.getDecoder().decode(sms);
 
 		try {
@@ -116,7 +113,7 @@ public class TestDecoding {
 	@Test
 	public void testDecodeTrackerEventeHA() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String sms = "nVFcVHTsLplka63tOuyVlFHBW2bSucap36MJdnEpJNK5Fl5Jy0WghAtNi0PYejm1HWBqePUDH5XFR07GPO1oW5iZGZoamwBAHqggmJiYmIB28ia6ubowszCQIbe3OjK0AGH2IzeyMLyQKbK5sLyAAA==";
+		String sms = "0VFceV7sLplka63tOuyVlFHBW2bSucap36MJdnEpJNK5Fl5Jy0WghAtNi0PYejm1HWBqePUDH5XHle7GPO1oW5iZGZoamwBAHqggmJiYmIB28ia6ubowszCQIbe3OjK0AGH2IzeyMLyQKbK5sLyAAA==";
 		byte[] smsBytes = Base64.getDecoder().decode(sms);
 
 		try {
